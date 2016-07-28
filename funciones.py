5f7219b0e5d89f2ae77db8bf71e2dae5533ccaa5
andrefuentes <andrefuentes166@gmail.com>
2016-07-28 14:43:46 -0600

    funciones

--- funciones.py
+++ funciones.py
@@ -1,13 +1,24 @@
-guiones=[]
-gano=false
-v=0
+
+
+while j= "si":
+	w=palabra
+	pal=list(w)
+	guiones=[]
+	gano=false
+	v=0
+	lu=[]
+	for l in w:
+		guiones.append( " _ ")
+while gano != "si" and gano != "no":
+	print(" " join(guiones))
+	l=input()
 letra=input("ingrese una letra")
 for i in range(len(palabra)):
 		guion[i]=letra
 		palabra[i]=letra	
 espacios=len(palabra)*"_"
 print (espacios)
-while intentos !=0:
+
 if letra=palabra:
 		print"letra"
 		elif letra not in palabra:
