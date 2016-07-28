a15669ce9f5f64ccc7e68d55241ae7f92c3f981b
andrefuentes <andrefuentes166@gmail.com>
2016-07-28 14:32:29 -0600

    funciones.py

---
+++ funciones.py
@@ -0,0 +1,15 @@
+
+letra=input("ingrese una letra")
+for i in range(len(palabra)):
+		guion[i]=letra
+		palabra[i]=letra	
+espacios=len(palabra)*"_"
+print (espacios)
+while intentos !=0:
+if letra=palabra:
+		print"letra"
+		elif letra not in palabra:
+			print cuerpo(0)
+	for letra in palabra:
+		print(guiones)
+
